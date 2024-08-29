4c7f0f1435561070765f01dbad925e9bfdc6f245
Santt4na <jhonnsantt4na@gmail.com>
2024-08-29 15:33:07 -0300

    Introdução ao C#

---
+++ Introducao.cs
@@ -0,0 +1,36 @@
+public class EnemyAI : MonoBehaviour
+{
+   // Variaveis : Podemos mudar o valor em tempo de execução
+   int health;
+   float speed;
+   string enemyName;
+   bool idDead;
+
+   // Comerça o metodo com o retorno 
+   // void não precisa retornar nada
+
+   void Movement()
+   {  // && = e
+      // || = Ou
+      if (speed > 0 && health > 0)
+      {
+
+      }
+   }
+   void Dead()
+   {
+      if (health > 0)
+      {
+
+      }
+      else
+      {
+
+      }
+   }
+
+   void Attack()
+   {
+
+   }
+}
