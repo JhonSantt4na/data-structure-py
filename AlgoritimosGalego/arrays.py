7556ad7c64b1011a632d13f7705b10ffca0c7349
Santt4na <jhonnsantt4na@gmail.com>
2025-03-08 22:01:40 -0300

    Array + Big O Notation

---
+++ AlgoritimosGalego/arrays.py
@@ -0,0 +1,12 @@
+
+#Nome do Array = [Valores do array ]
+array = [1,2,3,4,5,6,7,8,9]
+
+# Sendo que os indice comerca com 0
+# Neste caso array[4] = 5
+print(array[4]) # Return = 5
+
+if len(array) != 0:
+  print("Array Cheio")
+else:
+  print("Array Vazio")
